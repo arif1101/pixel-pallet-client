34b1803318d48480d69349e40b7f8f05081cf3f0
arif1101 <arifurrahmanarif223@gmail.com>
2025-10-01 10:08:48 +0600

    feat(ui): add recent work carousel on homepage

---
+++ src/data/portfolioItems.ts
@@ -0,0 +1,45 @@
+interface PortfolioItem {
+  id: number
+  title: string
+  category: string
+  image: string
+}
+
+export const portfolioItems: PortfolioItem[] = [
+  {
+    id: 1,
+    title: "Follio - Multipurpose Portfolio HTML5 Template",
+    category: "Web Design",
+    image: "/me.JPG",
+  },
+  {
+    id: 2,
+    title: "Elito - Creative Portfolio HTML5 Template",
+    category: "Website / Creative",
+    image: "/me.JPG",
+  },
+  {
+    id: 3,
+    title: "Arkio - Architecture & Interior WordPress Theme",
+    category: "Architecture / Business",
+    image: "/me.JPG",
+  },
+  {
+    id: 3,
+    title: "Arkio - Architecture & Interior WordPress Theme",
+    category: "Architecture / Business",
+    image: "/me.JPG",
+  },
+  {
+    id: 3,
+    title: "Arkio - Architecture & Interior WordPress Theme",
+    category: "Architecture / Business",
+    image: "/me.JPG",
+  },
+  {
+    id: 3,
+    title: "Arkio - Architecture & Interior WordPress Theme",
+    category: "Architecture / Business",
+    image: "/me.JPG",
+  }
+]
